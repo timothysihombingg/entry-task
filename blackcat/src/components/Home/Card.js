b705159ec4e6a693336134a3bf200ff5b393c564
Timothy Thamrin Andrew Sihombing <timothy.sihombing@C02CC6X4MD6M.local>
2020-06-14 23:40:46 +0700

    card container in home page

--- blackcat/src/components/Home/Card.js
+++ blackcat/src/components/Home/Card.js
@@ -1,29 +1,38 @@
-import React from 'react'
-import Image from '../../assets/images/gmap.png'
+import React from 'react';
+import Image from '../../assets/images/gmap.png';
+import Time from '../../assets/svgs/time.svg';
+import Check from '../../assets/svgs/check-outline.svg';
+import Like from '../../assets/svgs/like-outline.svg';
 
 import './Card.css'
 
 function Card() {
   return (
-    <div>
-      <div>
+    <div className="home-card">
+      <div className="user-name-container">
         <img id="card-usr-img" alt="user profile" src={Image} />
-        <div>Username</div>
-        <div>Channel name longer</div>
+        <div className="user-name">Username</div>
+        <div className="channel-name">Channel name longer</div>
       </div>
-      <p>Activity Title Name Make it Longer May Longer than One Line</p>
       <div>
-
-      </div>
-      <p>
-        [No longer than 300 chars] Vivamus sagittis, diam in lobortis, sapien arcu mattis erat, vel aliquet sem urna et risus. Ut feugiat sapien mi potenti...
-      </p>
-      <div>
-        <div>
-
+        <div className="card-title-date">
+          <div className="card-title">Activity Title Name Make it Longer May Longer than One Line</div>
+          <img id="card-time-img" alt="user profile" src={Time} />
+          <p className="card-date">14 May 2016 - 14 May 2016</p>
+          <p className="card-desc">
+            [No longer than 300 chars] Vivamus sagittis, diam in lobortis, sapien arcu mattis erat, vel aliquet sem urna et risus. Ut feugiat sapien mi potenti...
+          </p>
         </div>
-        <div>
-
+        {/* <img id="card-img" alt="user profile" src={Image} />  */}
+      </div>
+      <div>
+        <div className="card-like">
+          <img id="card-check-img" alt="user profile" src={Check} />
+          <p className="card-check">6 Going</p>
+        </div>
+        <div className="card-like">
+          <img id="card-like-img" alt="user profile" src={Like} />
+          <p className="card-check">10 Likes</p>
         </div>
       </div>
     </div>
